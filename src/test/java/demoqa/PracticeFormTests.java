1dcf482e50309d19fbc6d0cecf71ed1f8bb9bde7
Tamara Batyukova <tbatyukova@yandex.ru>
2023-08-24 00:06:32 +0400

    Added successfulRegistrationTest

--- src/test/java/demoqa/PracticeFormTests.java
+++ src/test/java/demoqa/PracticeFormTests.java
@@ -1,40 +1,66 @@
 package demoqa;
 
 import com.codeborne.selenide.Configuration;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class PracticeFormTests {
 
-
-
     @BeforeAll
-    static public void setup(){
-        WebDriverManager.chromedriver().setup();
-        Configuration.browser = "chrome";
-        Configuration.webdriverLogsEnabled = true;
+    static void beforAll(){
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = false;
+        Configuration.pageLoadTimeout = 60000;
+        Configuration.timeout = 60000;
 
     }
 
     @Test
-    void fillPracticeForm() {
-        open("https://demoqa.com/automation-practice-form");
+    void successfulRegistrationTest() {
+        open("/automation-practice-form");
         $(".main-header").shouldHave(text("Practice Form"));
-//        $(".practice-form-wrapper").$(byText("Student Registration Form"))
-//                .shouldHave(text("Student Registration Form"));
-//        $("#firstName").setValue("Igor");
-//        $("#lastName").setValue("Akinfeev");
-//        $("#userEmail").setValue("IgorAKIII@ya.ru");
-//        $("#gender-radio-1[value=Male]").click();
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").setValue("Igor");
+        $("#lastName").setValue("Akinfeev");
+        $("#userEmail").setValue("IgorAKIII@ya.ru");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("8889991234");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("2003");
+        $(".react-datepicker__day--003").click();
+
+        $("#subjectsInput").setValue("Chemistry").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("1.png");
+        $("#currentAddress").setValue("Some address 222");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+
+        $(".modal-dialog").should(appear);
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Igor"), text("Akinfeev"), text("IgorAKIII@ya.ru"), text("8889991234"));
+
+
+
+
+
+
 
 
     }
